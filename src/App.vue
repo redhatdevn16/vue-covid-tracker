c1213fe2307421867ad412623bbf186e0bcd8114
redhatdevn16 <redhatdevn16@gmail.com>
2023-08-08 23:30:55 +0800

    initialize

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Header />
+  <Home />
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Home from '@/components/Home';
+
+export default {
+  components: {
+    Header, Home
+  }
+}
+</script>
+
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+a {text-decoration: none;color:inherit}
+#app {
+  font-family: Arial, sans-serif;
+}
+.container {
+  width: 90%;
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
